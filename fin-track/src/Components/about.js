b68de8baaff735ded306967dadc1aa67371aa39a
Two-Ocean <73182679+Two-Ocean@users.noreply.github.com>
2024-03-22 10:50:51 +1300

    "Added about page"

---
+++ fin-track/src/Components/about.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import MainNavbar from "./mainNavbar";
+
+function About() {
+    return(
+        <div></div>
+    );
+}
+
+export default About;
