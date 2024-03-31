40a3658f3408c05b28ad19e599b341952035322a
abhishek-2110 <sharmabishek762@gmail.com>
2024-03-31 13:45:43 +1300

    AdminPage added

---
+++ fin-track/src/Components/AdminPage.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { doSignOut } from "./Firebase/Auth";
+import VectorLogo from "../Images/Vector_Logo_White.png";
+import "../CSS/userNavbar.css";
+ 
+function AdminPage() {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarTogglerDemo03"
+            aria-controls="navbarTogglerDemo03"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+ 
+          <Link to="/" className="navbar-brand">
+            <img src={VectorLogo} className="img-fluid" alt="brand-logo" />{" "}
+            FinTrack
+          </Link>
+ 
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+ 
+            <div className="d-flex">
+              {/* Call handleLogout function when the logout button is clicked */}
+              <button className="btn custom-btn" onClick={handleLogout}>
+                Log Out
+              </button>
+            </div>
+           
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
+ 
+export default AdminPage;
